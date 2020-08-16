fe7ff4ef86d5b379e598afc428909c6821e77924
DavidMosquera <jdmosquerat@unal.edu.co>
2020-08-15 23:24:53 -0500

    Adición de interfaz gráfica de usuario

--- reto_1/controlador.py
+++ reto_1/controlador.py
@@ -15,6 +15,27 @@ class ControladorInterface:
     #METODO PARA OBTENER LA COLUMNA DE LA POSICION ALEATORIA
     def obtenerColumnaFicha(self) -> str:
         pass
+    #METODO PARA OBTENER EL ALFIL
+    def obtenerTableroAlfil(self) -> str:
+        pass
+    #METODO PARA OBTENER EL CABALLO
+    def obtenerTableroCaballo(self) -> str:
+        pass
+    #METODO PARA OBTENER LA TORRE
+    def obtenerTableroTorre(self) -> str:
+        pass
+    #METODO PARA OBTENER EL PEON NEGRO
+    def obtenerTableroPeonNegro(self) -> str:
+        pass
+    #METODO PARA OBTENER EL PEON BLANCO
+    def obtenerTableroPeonBlanco(self) -> str:
+        pass
+    #METODO PARA OBTENER LA REINA
+    def obtenerTableroReina(self) -> str:
+        pass
+    #METODO PARA OBTENER EL REY
+    def obtenerTableroRey(self) -> str:
+        pass
 
 #CLASE DEL CONTROLADOR QUE IMPLEMENTA LA INTERFAZ
 class Controlador(ControladorInterface):
@@ -27,14 +48,22 @@ class Controlador(ControladorInterface):
         self.columnaFicha = rnd.randint(0,columnas-1)
         #LISTA DE TABLEROS
         self.tableros = []
+        #CREAMOS CADA TABLERO
+        self.tableroAlfil = md.TableroAlfil(filas, columnas, self.filaFicha, self.columnaFicha)
+        self.tableroCaballo = md.TableroCaballo(filas, columnas, self.filaFicha, self.columnaFicha)
+        self.tableroPeonBlanco = md.TableroPeonBlanco(filas, columnas, self.filaFicha, self.columnaFicha)
+        self.tableroPeonNegro = md.TableroPeonNegro(filas, columnas, self.filaFicha, self.columnaFicha)
+        self.tableroTorre = md.TableroTorre(filas, columnas, self.filaFicha, self.columnaFicha)
+        self.tableroRey = md.TableroRey(filas, columnas, self.filaFicha, self.columnaFicha)
+        self.tableroReina = md.TableroReina(filas, columnas, self.filaFicha, self.columnaFicha)
         #AGREGAMOS CADA TABLERO A LA LISTA
-        self.tableros.append(md.TableroAlfil(filas, columnas, self.filaFicha, self.columnaFicha))
-        self.tableros.append(md.TableroCaballo(filas, columnas, self.filaFicha, self.columnaFicha))
-        self.tableros.append(md.TableroPeonBlanco(filas, columnas, self.filaFicha, self.columnaFicha))
-        self.tableros.append(md.TableroPeonNegro(filas, columnas, self.filaFicha, self.columnaFicha))
-        self.tableros.append(md.TableroTorre(filas, columnas, self.filaFicha, self.columnaFicha))
-        self.tableros.append(md.TableroRey(filas, columnas, self.filaFicha, self.columnaFicha))
-        self.tableros.append(md.TableroReina(filas, columnas, self.filaFicha, self.columnaFicha))
+        self.tableros.append(self.tableroAlfil)
+        self.tableros.append(self.tableroCaballo)
+        self.tableros.append(self.tableroPeonBlanco)
+        self.tableros.append(self.tableroPeonNegro)
+        self.tableros.append(self.tableroTorre)
+        self.tableros.append(self.tableroRey)
+        self.tableros.append(self.tableroReina)
     
     #SIMULA LOS MOVIMIENTOS DE TODOS LOS TABLEROS EN LA LISTA DE TABLEROS
     def simularMovimientos(self):
@@ -55,3 +84,25 @@ class Controlador(ControladorInterface):
         for tablero in self.tableros:
             tableroStr += tablero.obtenerTablero() + " \n"
         return tableroStr
+    
+    #METODO PARA OBTENER EL ALFIL
+    def obtenerTableroAlfil(self) -> str:
+        return self.tableroAlfil.obtenerTablero()
+    #METODO PARA OBTENER EL CABALLO
+    def obtenerTableroCaballo(self) -> str:
+        return self.tableroCaballo.obtenerTablero()
+    #METODO PARA OBTENER LA TORRE
+    def obtenerTableroTorre(self) -> str:
+        return self.tableroTorre.obtenerTablero()
+    #METODO PARA OBTENER EL PEON NEGRO
+    def obtenerTableroPeonNegro(self) -> str:
+        return self.tableroPeonNegro.obtenerTablero()
+    #METODO PARA OBTENER EL PEON BLANCO
+    def obtenerTableroPeonBlanco(self) -> str:
+        return self.tableroPeonBlanco.obtenerTablero()
+    #METODO PARA OBTENER LA REINA
+    def obtenerTableroReina(self) -> str:
+        return self.tableroReina.obtenerTablero()
+    #METODO PARA OBTENER EL REY
+    def obtenerTableroRey(self) -> str:
+        return self.tableroRey.obtenerTablero()
--- reto_1/vista.py
+++ reto_1/vista.py
@@ -1,22 +1,78 @@
 import controlador as crl
-
+import tkinter as tk
+from tkinter import scrolledtext
 #INTERFAZ DE LA VISTA
 class VistaInterface:
     #METODO VACIO PARA INICIAR LA VISTA
     def iniciar(self):
         pass
+    #METODO VACIO PARA EL BOTON SIMULAR
+    def simular(self):
+        pass
     
 #CLASE VISTA
 class Vista(VistaInterface):
-    #INICIA EL CONTROLADOR
-    def __init__(self):
-        self.controlador = crl.Controlador(8,8)
-
-    #INICIA LA EJECUCION DE LA VISTA
-    def iniciar(self):
-        while(input("Simular [1]\nSalir [0]\n") == "1"):
-            self.controlador = crl.Controlador(8,8)
-            print("Posición de la ficha (fila,columna): (",self.controlador.obtenerFilaFicha(), ",", self.controlador.obtenerColumnaFicha(),")")
-            self.controlador.simularMovimientos()
-            print(self.controlador.obtenerString())
         
+    #INICIAR LA INTERFAZ
+    def iniciar(self):
+        
+        #CREACION DE LOS ELEMENTOS DE LA INTERFAZ GRAFICA DE USUARIO
+        ventana = tk.Tk()
+        ventana.title("[ULTRACOM] Simulador de posiciones-v3.0")
+        ventana.geometry("765x319")
+        
+        self.peonBlanco = scrolledtext.ScrolledText(ventana,width=20,height=9)
+        self.peonBlanco.grid(column=0,row=0, padx=5, pady=5)
+        
+        self.peonNegro = scrolledtext.ScrolledText(ventana,width=20,height=9)
+        self.peonNegro.grid(column=1,row=0, padx=5, pady=5)
+        
+        self.alfil = scrolledtext.ScrolledText(ventana,width=20,height=9)
+        self.alfil.grid(column=2,row=0, padx=5, pady=5)
+        
+        self.torre = scrolledtext.ScrolledText(ventana,width=20,height=9)
+        self.torre.grid(column=3,row=0, padx=5, pady=5)
+        
+        self.rey = scrolledtext.ScrolledText(ventana,width=20,height=9)
+        self.rey.grid(column=0,row=1, padx=5, pady=5)
+        
+        self.reina = scrolledtext.ScrolledText(ventana,width=20,height=9)
+        self.reina.grid(column=1,row=1, padx=5, pady=5)
+        
+        self.caballo = scrolledtext.ScrolledText(ventana,width=20,height=9)
+        self.caballo.grid(column=2,row=1, padx=5, pady=5)
+        
+        self.botonSimular = tk.Button(ventana, text ="Simular", command = self.simular)
+        self.botonSimular.grid(column=3,row=1, padx=5, pady=5)
+        
+        #SIMULACION INICIAL
+        self.simular()
+        
+        #LOOP PRINCIPAL
+        ventana.mainloop()
+        
+        
+    def simular(self):
+        #INICIAR UN NUEVO CONJUNTO DE TABLEROS
+        self.controlador = crl.Controlador(8,8)
+        self.controlador.simularMovimientos()
+        #BORRAR EL CONTENIDO DE LOS CAMPOS DE TEXTO
+        self.peonBlanco.delete(1.0,tk.END)
+        self.peonNegro.delete(1.0,tk.END)
+        self.alfil.delete(1.0,tk.END)
+        self.torre.delete(1.0,tk.END)
+        self.rey.delete(1.0,tk.END)
+        self.reina.delete(1.0,tk.END)
+        self.caballo.delete(1.0,tk.END)
+        #INSERTAR EL CONTENIDO DE LOS CAMPOS DE TEXTO
+        self.peonBlanco.insert(1.0,self.controlador.obtenerTableroPeonBlanco())
+        self.peonNegro.insert(1.0,self.controlador.obtenerTableroPeonNegro())
+        self.alfil.insert(1.0,self.controlador.obtenerTableroAlfil())
+        self.torre.insert(1.0,self.controlador.obtenerTableroTorre())
+        self.rey.insert(1.0,self.controlador.obtenerTableroRey())
+        self.reina.insert(1.0,self.controlador.obtenerTableroReina())
+        self.caballo.insert(1.0,self.controlador.obtenerTableroCaballo())
+        
+if __name__ == "__main__":
+    vista = Vista()
+    vista.iniciar()
